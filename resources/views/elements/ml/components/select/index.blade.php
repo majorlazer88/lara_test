22e09e6cab237ab4499dbf5115161b0dc42b2339
Mihhail <mihhailk88@gmail.com>
2022-10-21 17:37:14 +0300

    style blade components

--- resources/views/elements/ml/components/select/index.blade.php
+++ resources/views/elements/ml/components/select/index.blade.php
@@ -1,5 +1,5 @@
 @props(['mt' => 'mt-1', 'id', 'name'])
 
-<select {{$attributes->merge(['class' => $mt])}} id="{{$id}}" name={{$name}}>
+<select {{$attributes->class('!border-sky-400')->merge(['class' => $mt,])}} id="{{$id}}" name={{$name}}>
     {{$slot}}
 </select>
