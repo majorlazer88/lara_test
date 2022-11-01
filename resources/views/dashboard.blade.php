edc1ee8d39144b61af1307f24f36394feeb2245b
Mihhail <mihhailk88@gmail.com>
2022-11-01 16:06:35 +0200

    display info text if links variable is empty

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,18 @@
         What next?!
     </x-slot>
     <hr/>
-    <label>Select link
-        <x-ml-html-elements::select id="selectLink" name="select-link">
-            @foreach ($links as $link)
-                @php $selected = $loop->first === $loop->index ? 'selected' : '' @endphp
-                <x-ml-html-elements::select.option :index="$loop->index" :selected="$selected">
-                    {{$link->domain . ' Id: ' . $loop->index . ' ' . 'Price: ' . $link->platform_price . ''}}
-                </x-ml-html-elements::select.option>
-            @endforeach
-        </x-ml-html-elements::select>
-    </label>
+    @if(!empty($links))
+        <label>Select link
+            <x-ml-html-elements::select id="selectLink" name="select-link">
+                @foreach ($links as $link)
+                    @php $selected = $loop->first === $loop->index ? 'selected' : '' @endphp
+                    <x-ml-html-elements::select.option :index="$loop->index" :selected="$selected">
+                        {{$link->domain . ' Id: ' . $loop->index . ' ' . 'Price: ' . $link->platform_price . ''}}
+                    </x-ml-html-elements::select.option>
+                @endforeach
+            </x-ml-html-elements::select>
+        </label>
+    @else
+        <div>No records in database</div>
+    @endif
 </x-ml-app-layout>
